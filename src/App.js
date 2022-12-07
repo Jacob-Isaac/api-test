e5b426c5a6b414b7c4edc147fe59a8f9b7e520f8
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-12-07 10:17:53 +0100

    "hello world" added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          WITAJ ŚWIECIE !!!!!!!!!!!!!!!!!!!!!
         </a>
       </header>
     </div>
